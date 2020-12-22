c78c10cf952fcefeda73e74d4c7881684546ebc2
suzenghao <ccrrab@qq.com>
2020-12-23 01:06:16 +0800

    Initial commit

---
+++ data/user_manager_data.py
@@ -0,0 +1,14 @@
+
+
+
+class UserMangerData():
+
+
+    user_case_data = {
+        "username": "test118",
+        "password": "test118",
+        "new_username": "xxxx003",
+        "new_password": "xxxx003"
+    }
+
+
